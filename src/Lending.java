2a5b50bb48761db2687a9d52dfe0fbae39171d15
KhanhDT2410 <156813143+KhanhDT2410@users.noreply.github.com>
2024-10-06 19:01:22 +0700

    Lending.java

--- src/Lending.java
+++ src/Lending.java
@@ -1,3 +1,23 @@
-public class Lending {
-    
+
+import java.time.LocalDate;
+
+class Lending {
+    String bcode;  // Book code
+    String rcode;  // Reader code
+    int state;     // 1: Book is lent, 2: Returned
+    LocalDate ldate;  // Lending date
+    LocalDate rdate;  // Return date
+
+    public Lending(String bcode, String rcode, int state, LocalDate ldate, LocalDate rdate) {
+        this.bcode = bcode;
+        this.rcode = rcode;
+        this.state = state;
+        this.ldate = ldate;
+        this.rdate = rdate;
+    }
+
+    @Override
+    public String toString() {
+        return bcode + ", " + rcode + ", " + state + ", " + ldate + ", " + (rdate != null ? rdate : "null");
+    }
 }
